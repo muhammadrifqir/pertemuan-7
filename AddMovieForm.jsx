3194232b9cfef8883f24894cd4697948e0bef192
muhammadrifqir <148336833+muhammadrifqir@users.noreply.github.com>
2024-06-02 10:36:45 +0700

    Update AddMovieForm.jsx

--- AddMovieForm.jsx
+++ AddMovieForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // import component alert
 import Alert from "../Components/Alert/Alert";
+import { nanoid } from "nanoid";
 
 function AddMovieForm(props) {
   /**
@@ -8,114 +9,87 @@ function AddMovieForm(props) {
    * kode yang lainnya tetap sama
    */
 
+  // membuat state: istitleerror, isdateerror
+  const [IsTitleError, setIsTitleError] = useState(false);
+  const [IsDateError, setIsDateError] = useState(false);
+  
   // membuat state title
-  const [isTitleError, setIsTitleError] = useState(false);
-  const [isDateError, setIsDateError] = useState(false);
-}
-
-function Alert(props) {
-  /**
-   * children adalah props khusus
-   * berisi konten/children yang ada di dalam component
-   */
-  const {children} = props;
-
-  return (
-    <div>
-      <span>{children}</span>
-    </div>
-  )
-}
-
-
+  const [title, setTitle] = useState("");
 
   /**
-   * membuat fungsi handleTitle
+   * membuat fungsi handtitle 
    * dijalankan ketika nilai input berubah
    */
+
   function handleTitle(e) {
     /**
-     * jalankan fungsi setTitle
+     * jalankan fungsi settitle
      * set title nilai baru: input saat ini
      */
-    setTitle(e.target.value);
-  }
+    return (
+      <input
+        id="title"
+        type="text"
+        // memberikan value input: title
+        value={title}
+        //memberikan event onChange
+        onChange={handleTitle}
+      />
+      )
+  
+      /**
+       * membuat inline if: operator &&
+       * jika istitleerror true maka render error
+       */
+      {IsTitleError && <Alert>title wajib diisi</Alert>}
+      setTitle(e.target.value);
 
-  // membuat state date
-  const [date, setDate] = useState("");
-  /**
-   * membuat fungsi handledate
-   * dijalankan ketika nilai input berubah
-   */
+      function handleDate(e) {
+        /**
+         * jalankan fungsi setdate
+         * set date nilai baru: input saat ini
+         */
+        return (
+          <input
+            id="date"
+            type="text"
+            // memberikan value input: date
+            value={date}
+            //memberikan event onChange
+            onChange={handleDate}
+          />
+        )
+        /**
+       * membuat inline if: operator &&
+       * jika istitleerror true maka render error
+       */
+      {IsDateError && <Alert>date wajib diisi</Alert>}
+        setDate(e.target.value);
+      }
 
-  function handleDate(e) {
-    /**
-     * jalankan fungsi setDate
-     * set data nilai baru: input saat ini
-     */
-    setDate(e.target.value);
-  }
+      function handleSubmit (e) {
+        // jika title kosong, set istitleerror ,menjadi true
+        if (title === "") {
+          setIsTitleError(true);
+        }
+        // jika date kosong, set isdateerror menjadi true
+        else if (date === "") {
+          setIsDateError(true);
+        }
+        // jika tidak, push movie dan set eror menjadi false
+        else {
+          const movie = {
+            id: nanoid(), title: title,
+            year: date, type: "Movie",
+            poster: "https://picsum.photos/300/400",
+          };
+          setMovies([...movies, movie]);
 
-  return (
-    <input
-      id="date"
-      type="text"
-      // memberikan value input: date
-      value={date}
-      //memberikan event onChange
-      onChange={handleDate}
-    >
-    /**
-    * membuat inline if: operator &&
-    * jika istitleeror true maka render eror
-    */
+          // set eror menjadi false
+          setIsTitleError(false);
+          setIsDateError(false);
+        }
+      }
+  };
 
-      {isDateError && <Alert>Date Wajib Diisi</Alert> }
-    </input>
-  );
-
-  function handleSubmit(e) {
-    // jika title kosong, set isTitleeror menjadi true
-    if (title === "") {
-      setIsTitleError(true);
-    }
-    // jika date kosong, set isdateeror menjadi true
-    else if (date === "") {
-      setIsDateError(true);
-    }
-    // jika tidak, push movie dan set eror menjadi false
-    else {
-    const movie = {
-      id: nanoid(), title: title,
-      year: date, type: "Movie",
-      poster: "https://picsum.photod/300/400",
-    };
-    setMovies([...movies, ,movie]);
-
-    // set eror menjadi false
-    setIsTitleError(false);
-    setIsDateError(false);
-    }
-  }
-
-  return <form onSubmit={handleSubmit}></form>;
-}
-return (
-  <input
-    id="title"
-    type="text"
-    // memberikan value input: title
-    value={title}
-    //memberikan event onChange
-    onChange={handleTitle}
-  >
-    /**
-    * membuat inline if: operator &&
-    * jika istitleeror true maka render eror
-    */
-    {isTitleError && <Alert>Title Wajib Diisi</Alert>}
-    
-  </input>
-);
-
-export default Alert;
+export default AddMovieForm;
